f970fc766a2e67ebe5960b4d1a8176ae71989482
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2025-03-15 12:36:47 +0100

    add LMStudio support in Generator

--- src/Generation/Generator.py
+++ src/Generation/Generator.py
@@ -1,3 +1,4 @@
+from lmstudio._sdk_models import RepositoryChannelPushArtifactToClientPacketMessageDict
 import ollama
 import lmstudio as lms
 
@@ -42,8 +43,7 @@ def gen_response_oll_stream(gen_model_name: str, query: str, context: list[str])
     messages = [
         {
             "role": "system",
-            "content": """Du bist ein hilfreicher KI-Assistent einer Universität, der darauf spezialisiert ist,
-            die Fragen von Studierenden zu Ihrer Studienordnung auf Grundlage von bereitgestellten Abschnitten der Studienordnung zu Beantworten.
+            "content": """Du bist ein hilfreicher KI-Assistent einer Universität, der darauf spezialisiert ist, die Fragen von Studierenden zu Ihrer Studienordnung auf Grundlage von bereitgestellten Abschnitten der Studienordnung zu Beantworten.
             Befolge diese Regeln:
             1. Verwende ausschließlich Informationen aus den bereitgestellten Abschnitten der Studienordnung.
             2. Wenn du die Antwort in den bereitgestellten Abschnitten nicht findest, sage es direkt.
@@ -77,31 +77,27 @@ def gen_response_oll_stream(gen_model_name: str, query: str, context: list[str])
 
 def gen_response_lms_stream(model_name: str, query: str, context: list[str]):
     context_text = "\n".join(context)
-    messages = [
-        {
-            "role": "system",
-            "content": """Du bist ein hilfreicher KI-Assistent einer Universität, der darauf spezialisiert ist,
-            die Fragen von Studierenden zu Ihrer Studienordnung auf Grundlage von bereitgestellten Abschnitten der Studienordnung zu Beantworten.
-            Befolge diese Regeln:
-            1. Verwende ausschließlich Informationen aus den bereitgestellten Abschnitten der Studienordnung.
-            2. Wenn du die Antwort in den bereitgestellten Abschnitten nicht findest, sage es direkt.
-            3. Sei präzise und direkt in deinen Antworten und benutze nur diejenigen Abschnitte, die relevant für die Beantwortung der Frage sind.
-            4. Wenn du aus der Studienordnung zitierst, erwähne dies, indem du die den Paragraphen und gegebenenfalls Absatz und Punkt (falls vorhanden) am Ende deiner Antwort in Klammern angibst.
-            5. Antworte in der gleichen Sprache, in der die Frage gestellt wurde."""
-        },
-        {
-            "role": "user",
-            "content": f"""Hier sind die Abschnitte für die Beantwortung:
+    model = lms.llm(model_name)
 
-            {context_text}
+    chat = lms.Chat("""Du bist ein hilfreicher KI-Assistent einer Universität, der darauf spezialisiert ist, die Fragen von Studierenden zu Ihrer Studienordnung auf Grundlage von bereitgestellten Abschnitten der Studienordnung zu Beantworten.
 
-            Beantworte auf Grundlage der Abschnitte folgende Frage: {query}"""
-        }
-    ]
-    with lms.Client() as client:
-         model = client.llm.model("mistral-nemo-instruct-2407")
-    lms.ChatHistoryDataDict()
-    result = lms.Chat.from_history({"messages": [
-      { "role": "system", "content": "You are a resident AI philosopher." },
-      { "role": "user", "content": "What is the meaning of life?" },
-    ]})
+    Befolge diese Regeln:
+    1. Verwende ausschließlich Informationen aus den bereitgestellten Abschnitten der Studienordnung.
+    2. Wenn du die Antwort in den bereitgestellten Abschnitten nicht findest, sage es direkt.
+    3. Sei präzise und direkt in deinen Antworten und benutze nur diejenigen Abschnitte, die relevant für die Beantwortung der Frage sind.
+    4. Wenn du aus der Studienordnung zitierst, erwähne dies, indem du die den Paragraphen und gegebenenfalls Absatz und Punkt (falls vorhanden) am Ende deiner Antwort in Klammern angibst.
+    5. Antworte in der gleichen Sprache, in der die Frage gestellt wurde.""")
+
+    chat.add_user_message(f"""Hier sind die Abschnitte für die Beantwortung:
+
+    {context_text}
+
+    Beantworte auf Grundlage der Abschnitte folgende Frage: {query}""")
+
+    response_stream = model.respond_stream(chat, config={
+        "temperature": 0.6,
+        "topPSampling": 0.95
+    })
+
+    for fragment in response_stream:
+        yield fragment.content
